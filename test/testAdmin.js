2b2d19738cf3cf3243188232c6275bee8f641727
Cristian Vargas <cristianv@gentem.org>
2020-04-01 00:52:31 -0500

    Se realizan ajustes en el test, para que el mismo test instancie el servidor

--- test/testAdmin.js
+++ test/testAdmin.js
@@ -1,13 +1,14 @@
 let chai = require('chai');
+app = require('../app');
 let clientChaiHtpp = require("chai-http");
 const expect = require('chai').expect;
 
 chai.use(clientChaiHtpp);
-const url = `http://localhost:3000`;
+
 
 describe('create a admin', function(){
     it('Should create a admin', function(done){
-        chai.request(url).post("/users/test")
+        chai.request(app).post("/users/test")
         .send({id: 0})
         .end(function(err, res){
             expect(res).to.have.status(200);
