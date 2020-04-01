065af77de60641e03cd27180f423ce494603baee
Cristian Vargas <cristianv@gentem.org>
2020-03-31 21:07:28 -0500

    se ha añadido el codigo para mantener una comunicacion segura entre el cliente y el servidor atravez de JWT json web token

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
+
 var router = express.Router();
 
-/* GET users listing. */
-router.post('/test', function(req, res, next) {
+function midelwareTokenAuth(req, res, next){
+  const token = req.headers['access-token'];
+  if (token) {
+    jwt.verify(token, process.env.TOKEN, function(err, decoded){
+      if(err) res.json({message: "invalid-Token"});
+      req.decoded = decoded;
+      next();
+    });
+  }else{
+    res.json({ message: "no-Token"});
+  }
+} 
+
+router.post('/auth-token', function(req, res, next){
+  const userdata  = { user: req.body.user, password: req.body.password };
+  const token = jwt.sign(userdata, process.env.TOKEN , {expiresIn: 1440});
+  res.json({token : token});
+});
+
+router.post('/test', midelwareTokenAuth ,function(req, res, next) {
   console.log("req");
   res.status(200).send("ok");
 });
