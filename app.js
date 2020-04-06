656fbc215e0cba20ed72b5f0ef939c0b91a4b5bc
Cristian Vargas <cristianv@gentem.org>
2020-04-06 16:16:02 -0500

    Se añadio mysql y se trabajo en los comandos SQL para el archivo ManagerConnection, se hizo un ejemplo de consulta a la BD y envio de datcliente web

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
