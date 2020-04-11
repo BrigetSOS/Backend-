f7196cd60d97ba69a8766a37ace57ae43608c5a5
Cristian Vargas <cristianv@gentem.org>
2020-04-11 02:26:31 -0500

    Se añadio la funcionalidad de crear administradores del sistema

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,23 +1,34 @@
 
+const Admin = require('../models/AdminModel').AdminModel;
 
 class AdminController {
 
-    constructor(){
-
+    constructor(data){
+        this.admin =  new Admin();
+        this.admin.setData(data);
     }
 
-    createAdmin(){
-
+    async createAdmin(){
+        return await this.admin.create();
     }
 
     deleteAdmin(){
-
+        return this.admin.delete();
     }
 
     editAdmin(){
+        return this.admin.edit();
+    }
 
+    validate(){
+        console.log("validate", this.admin.getName());
+        if(this.admin.getName() != undefined && this.admin.getEmail() != undefined) return true;
+        return false;
     }
 
 }
 
 
+module.exports = {
+    AdminController: AdminController
+}
--- models/AdminModel.js
+++ models/AdminModel.js
@@ -1,10 +1,40 @@
+const Database = require('../infrastructure/ManagerConnection').Connection;
+
 class AdminModel{
     constructor(){
+        this.database = new Database();
+        this.database.getConection();
+        this.name = "";
+        this.email = "";
+    }
+
+    async create(){
+     let result = await this.database.queryCommand(`INSERT INTO gentem.admin (name, email)
+        values ("${this.name}", "${this.email}")`);
+
+        return result['affectedRows'] == 1 ? 'created' :  'not-created';
+    }
+    
+    getDataAdmin(commandSQL){
 
     }
 
-    getDataAdmin(){
-
+    setData(data){
+        this.name = data.name;
+        this.email = data.email;
     }
 
+    getName(){
+        return this.name;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
+
+}
+
+module.exports = {
+    AdminModel :  AdminModel
 }
