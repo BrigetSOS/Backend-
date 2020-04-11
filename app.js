bac06e26b25a826c8a0109018e58e95aab1ade9e
Cristian Vargas <cristianv@gentem.org>
2020-04-10 21:25:36 -0500

    se añadieron los cors, para la la respuesta al cliente y no ocurra ningun problema en la respuesta al cliente

--- app.js
+++ app.js
@@ -11,12 +11,22 @@ var adminRouter = require('./routes/admin');
 var app = express();
 dotenv.config();
 
+function cors(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, '+
+    'X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
